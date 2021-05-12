600ec3ea62c63d358bc6132a7ebe29ec011aab49
SerhioAndros <androsiuksv@gmail.com>
2021-05-13 02:18:36 +0300

    Finalization of HW 10

--- src/index.js
+++ src/index.js
@@ -1,15 +1,51 @@
 import "./styles.css";
 
-import "./js/test";
+import menu from './menu.json';
 
-import arr from './menu.json';
+import parsMenu from './templates/menu.hbs';
 
-// import print from "./js/test"
+const refs = {
+    listNode: document.querySelector(".js-menu"),
+    themeSwitch: document.querySelector("#theme-switch-toggle"),
+    body: document.body,
+}
+console.log(refs);
+const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+};
 
-import * as obj from "./js/test";
+refs.listNode.innerHTML = menu.map(dish => parsMenu(dish));
 
-console.log(obj)
-// console.log(print);
+if (localStorage.getItem("theme") === "dark-theme") {
+    refs.body.classList.add(Theme.DARK);
+    refs.themeSwitch.checked = true;
+}
+else {
+    refs.body.classList.add(Theme.LIGHT);
+    refs.themeSwitch.checked = false;
+};
 
-// print(arr.map(e => e.name))
+refs.themeSwitch.addEventListener('change', themeCheck)
+
+function themeCheck(e) {
+    refs.body.classList.toggle(Theme.LIGHT);
+    refs.body.classList.toggle(Theme.DARK);
+    localStorage.setItem('theme', refs.body.classList)
+};
+
+
+// Альтернативный вариант (через if)
+
+// const changeTheme = function (oldTheme, newTheme) {
+//     refs.body.classList.remove(oldTheme)
+//     refs.body.classList.add(newTheme)
+// }
+// function themeChech(e) {
+//     if (refs.body.classList.contains(Theme.LIGHT)) {
+//         changeTheme(Theme.LIGHT, Theme.DARK)
+//     } else {
+//         changeTheme(Theme.DARK, Theme.LIGHT)
+//     }
+// };
 
