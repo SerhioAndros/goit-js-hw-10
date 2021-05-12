e0a85f8d133537319a1970635270270a0b1f0857
SerhioAndros <androsiuksv@gmail.com>
2021-05-12 23:20:06 +0300

    test commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+import "./js/test";
+
+import arr from './menu.json';
+
+// import print from "./js/test"
+
+import * as obj from "./js/test";
+
+console.log(obj)
+// console.log(print);
+
+// print(arr.map(e => e.name))
+
